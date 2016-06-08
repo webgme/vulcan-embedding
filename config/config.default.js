59d14c03cd311a7ee4a264c43443e7dfbcba66f0
pmeijer <patrik85@isis.vanderbilt.edu>
2016-06-08 23:41:21 +0200

    Add authorizer placeholder

--- config/config.default.js
+++ config/config.default.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var config = require('./config.webgme'),
+var path = require('path'),
+    config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
 // Add/overwrite any additional settings here
-// config.server.port = 8080;
-// config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
-
+config.authentication.enable = true;
+config.authentication.authorizer.path = path.join(__dirname, '../src/auth/authorizer');
 validateConfig(config);
 module.exports = config;
