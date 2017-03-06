3170814dde9ebff893015b30a46cd046f2be7020
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-03-06 12:01:38 -0600

    WIP Add extraCss class for vulcan.

--- config/config.default.js
+++ config/config.default.js
@@ -7,5 +7,6 @@ var path = require('path'),
 // Add/overwrite any additional settings here
 config.authentication.enable = true;
 config.authentication.authorizer.path = path.join(__dirname, '../src/auth/authorizer');
+config.visualization.extraCss.push('./vulcan-example/vulcan.css');
 validateConfig(config);
 module.exports = config;
